a56f28883a143930ee2a709f11e761c00977917b
w1sp <semen2002bs@gmail.com>
2023-09-19 09:13:36 +0300

    init commit

---
+++ app/src/main/java/com/example/bss_android_lb1_3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bss_android_lb1_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
